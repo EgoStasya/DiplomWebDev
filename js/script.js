da997b90c06373856d1fa127bb82a2c9215ff916
EgoStasya <50591846+EgoStasya@users.noreply.github.com>
2020-05-28 22:26:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){  
+  
+  $('.menu-burger').on('click', function() {
+
+    $(this).toggleClass('menu-burger_close');
+
+    $('.header-content__menu').slideToggle(200, function() {
+
+      if($(this).css('display') === 'none') {
+        $(this).removeAttr('style');
+      }
+      
+    });
+    
+  });  
+
+  $('.btn').on('click', function () {
+
+    var titleValue = $(this).val();   
+  
+    document.querySelector('.callback-form__title').innerHTML = titleValue;
+
+    $('.popup-page').fadeIn();
+    $('body').css('overflow', 'hidden');
+
+  });
+
+  $('.popup-block__btn').on('click', function () { 
+    $('.popup-page').fadeOut();
+    $('body').css('overflow', 'auto');
+  });
+
+  $('.popup-page').on('click', function (e) {
+    if (e.target == this) $('.popup-page').fadeOut();
+    $('body').css('overflow', 'auto');
+});
+
+
+  $("a[href^='#']").on('click', function(){
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top + 'px'
+    }, {
+      duration: 1000,
+      easing: 'swing'
+    });
+    return false;
+  });  
+
+
+  $('.callback-form__phone').inputmask({"mask": "+7 (999) 999-9999"});
+  
+  $('form').each(function () {
+    $(this).validate({      
+      rules: {
+        Телефон: {
+          required: true,
+        },
+        Имя: {
+          required: true,          
+        }
+      },
+      messages: {
+        Телефон: {
+          required: 'Обязательное поле для заполнения'
+        },
+        Имя: {
+          required: 'Обязательное поле для заполнения'
+        }
+      },
+      submitHandler(form) {
+        let th = $(form);
+
+        $.ajax({
+          type: 'POST',
+          url: 'mail.php',
+          data: th.serialize(),
+          
+        }).done(() => {
+            alert ('Ваша заявка принята. Мы свяжемся с вами в ближайше время!');
+            $('.popup-page').fadeOut();
+            $('body').css('overflow', 'auto');
+
+            th.trigger('reset');
+
+          });
+          
+        return false;
+      }
+    });
+    
+  });
+
+});
